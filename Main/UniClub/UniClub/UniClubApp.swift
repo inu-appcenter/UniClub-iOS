0612dd0014cf6f7128047f69599b0e06ffdeffa6
seunghwa04 <seunghwa0423@gmail.com>
2025-08-03 22:11:12 +0900

    add new file

---
+++ Main/UniClub/UniClub/UniClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniClubApp.swift
+//  UniClub
+//
+//  Created by seunghwa on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct UniClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
