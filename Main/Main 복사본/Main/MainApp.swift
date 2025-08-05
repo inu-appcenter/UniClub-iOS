940ff88b4ff24297bac935d28f015d4dc9647818
JeWookYeon <kihotemper554@gmail.com>
2025-08-05 10:28:23 +0900

    DS파일 삭제, main파일에 새롭게 병합

---
+++ Main/Main 복사본/Main/MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainApp.swift
+//  Main
+//
+//  Created by 제욱 on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
